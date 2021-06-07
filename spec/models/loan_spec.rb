7ff2f8b8df5cfb3c4ea0fc6a4cb76b686c0a8a06
Brian Guacarán <brianguacaran@gmail.com>
2021-06-07 14:01:04 -0400

    Added some rspec tests to Loan model

---
+++ spec/models/loan_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Loan, type: :model do
+  describe 'when is new record' do
+    before(:each) { subject.validate }
+
+    context 'with invalid attributes' do
+      it 'is invalid' do
+        expect(subject.invalid?).to be true
+      end
+
+      it 'has errors' do
+        expect(subject.errors).to_not be_empty
+      end
+    end
+
+    context 'without an amount' do
+      it 'has an amount related error' do
+        expect(subject.errors).to have_key(:amount)
+      end
+    end
+
+    context 'without an associated loan_type' do
+      it 'has a loan_type related error' do
+        expect(subject.errors).to have_key(:loan_type)
+      end
+    end
+
+    context 'with all valid attributes' do
+      subject { build(:loan) }
+
+      it 'is valid' do
+        expect(subject.valid?).to be true
+      end
+
+      it 'has an amount' do
+        expect(subject.amount.present?).to be true
+      end
+
+      it 'has an numeric amount' do
+        expect(subject.amount).to be_a(Numeric)
+      end
+
+      it 'has an associated loan_type' do
+        expect(subject.loan_type).to be_instance_of(LoanType)
+      end
+
+      context 'is saved' do
+        before(:each) { subject.save }
+        it 'is persisted' do
+          expect(subject.persisted?).to be true
+        end
+      end
+    end
+  end
+end
